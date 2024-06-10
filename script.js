f5d4bdc5bc160eedcfea4d4ebf88ae16bc4055cc
leandrobione <leandro.bione.92@gmail.com>
2024-06-10 00:24:50 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const countElement = document.getElementById('count');
+    const incrementButton = document.getElementById('increment');
+    const decrementButton = document.getElementById('decrement');
+
+    let count = 0;
+
+    incrementButton.addEventListener('click', () => {
+        count++;
+        countElement.textContent = count;
+    });
+
+    decrementButton.addEventListener('click', () => {
+        count--;
+        countElement.textContent = count;
+    });
+});
